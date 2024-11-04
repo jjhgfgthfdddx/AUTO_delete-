99dd1d133abb280380c30548d256fc557d1d5b73
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-11-04 21:16:09 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -21,7 +21,7 @@ import os
 API_ID       = int(os.environ.get("API_ID", "12618934"))
 API_HASH     = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
 BOT_TOKEN    = os.environ.get("BOT_TOKEN", "7587045703:AAGIOLfj4mSe8H1rmyqLWtdrotbpnQHewDY")
-SESSION      = os.environ.get("SESSION", "")
+SESSION      = os.environ.get("SESSION", "BQG1_SUAIzL4MN7ryZsmM3RF23ERZ5R3xxBnfU4lzQuih2ODJfFkCdQqOG5EOQVxou1dIIftnHdU0xQDl4bdCvs3mNSO3q5b3Z1T5N3_2SlpngGF-3ypEyJXI1ALI-JTsAF0p3y7qifi1maH6PzqHUda-wu0FsawdjLO3Xs1jjbApSpS620PelLgkO_iSxzAK4XhbYtNmn9fdcVqXp8MCvdBRtbDfoPCEt8hDmjyzjjqJXT6S10HtCe3v9ZLquaKx_MaFrk2Hlnta-rPVPnacrXirgj4H_qqgAq44wmtuUxC_Hk6UD6Wb3bCHNpp9OL8JvKX-AAoch8b7Sg9tc2_0XYGWrWCLQAAAAHFiGJyAA")
 TIME         = int(os.environ.get("TIME", 10))
 CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
