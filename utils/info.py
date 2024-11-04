3033dfb1a74581f2346168fa942cdd3fe3e88a3c
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-11-04 21:34:58 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -22,8 +22,8 @@ API_ID       = int(os.environ.get("API_ID", "19071424"))
 API_HASH     = os.environ.get("API_HASH", "c4b3e298cc50fd4cc563ae75ee882948")
 BOT_TOKEN    = os.environ.get("BOT_TOKEN", "6724470726:AAEqA0FQME2Bh7DVN2dJhpQdwOM3iMEA7l0")
 SESSION      = os.environ.get("SESSION", "BQG1_SUAIzL4MN7ryZsmM3RF23ERZ5R3xxBnfU4lzQuih2ODJfFkCdQqOG5EOQVxou1dIIftnHdU0xQDl4bdCvs3mNSO3q5b3Z1T5N3_2SlpngGF-3ypEyJXI1ALI-JTsAF0p3y7qifi1maH6PzqHUda-wu0FsawdjLO3Xs1jjbApSpS620PelLgkO_iSxzAK4XhbYtNmn9fdcVqXp8MCvdBRtbDfoPCEt8hDmjyzjjqJXT6S10HtCe3v9ZLquaKx_MaFrk2Hlnta-rPVPnacrXirgj4H_qqgAq44wmtuUxC_Hk6UD6Wb3bCHNpp9OL8JvKX-AAoch8b7Sg9tc2_0XYGWrWCLQAAAAHFiGJyAA")
-TIME         = int(os.environ.get("TIME", 10))
-CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
+TIME         = int(os.environ.get("TIME", 1))
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1002471728693").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Nazriya:Nazriya@cluster0.tkpcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
