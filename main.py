99b13f672cf86377693869df025eeced9422d1d3
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-11-05 21:35:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,5 +18,4 @@ async def start(user, message):
 #==========================================================
 
 Popen(f"gunicorn utils.server:app --bind 0.0.0.0:{PORT}", shell=True)
-Popen("python3 -m utils.delete", shell=True)
 User.run()
